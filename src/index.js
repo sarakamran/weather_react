0a14ceb67e6772e1e42df8fb1773cb3285873e7a
Sara Kamran <sara.kamran72@gmail.com>
2023-05-30 18:36:27 +0330

    add file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Weatherinfo from "./Weatherinfo";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="main">
+      <h1> Weather App </h1>
+      <Weatherinfo />
+      <small>
+        <a href="https://github.com/sarakamran/react-weather" target="_blank">
+          Open-source code
+        </a>
+        , by <a href="/">Sara Kamran</a>
+      </small>
+    </div>
   </React.StrictMode>
 );
 
